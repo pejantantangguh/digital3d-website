f6f43cdd361762bd005d3a6bd9826f8f55f39a05
pejantantangguh <suherman.ng@gmail.com>
2020-06-22 22:03:47 +1000

    Adding slider

--- assets/app.js
+++ assets/app.js
@@ -18179,6 +18179,38 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+var $ = jquery__WEBPACK_IMPORTED_MODULE_0___default.a; // ------------------------------------------------------- //
+//   Make a sticky navbar on scrolling
+// ------------------------------------------------------ //
+
+$(window).scroll(function () {
+  var body = $('body'),
+      stickyNavbar = $('nav.navbar-sticky'),
+      header = $('.header'),
+      topbar = $('.top-bar');
+
+  function makeItFixed(x) {
+    if ($(window).scrollTop() > x) {
+      stickyNavbar.addClass('fixed-top');
+
+      if (!header.hasClass('header-absolute')) {
+        body.css('padding-top', stickyNavbar.outerHeight());
+        body.data('smooth-scroll-offset', stickyNavbar.outerHeight());
+      }
+    } else {
+      stickyNavbar.removeClass('fixed-top');
+      body.css('padding-top', '0');
+    }
+  } // if .top-bar exists, affix the navbar when we scroll past .top-bar
+  // else affix it immediately
+
+
+  if (stickyNavbar.length > 0 && topbar.length > 0) {
+    makeItFixed(topbar.outerHeight());
+  } else if (stickyNavbar.length > 0) {
+    makeItFixed(0);
+  }
+});
 
 /***/ }),
 
@@ -18200,8 +18232,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/suhermanng/Programming/shopify/src/js/app.js */"./src/js/app.js");
-module.exports = __webpack_require__(/*! /Users/suhermanng/Programming/shopify/src/scss/app.scss */"./src/scss/app.scss");
+__webpack_require__(/*! /Users/suhermanng/Programming/digital3d-shopify/digital3dtheme/src/js/app.js */"./src/js/app.js");
+module.exports = __webpack_require__(/*! /Users/suhermanng/Programming/digital3d-shopify/digital3dtheme/src/scss/app.scss */"./src/scss/app.scss");
 
 
 /***/ })
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,36 @@
-import 'jquery';
+import jquery from 'jquery';
 import 'popper.js';
 import 'bootstrap';
+
+const $ = jquery;
+    // ------------------------------------------------------- //
+    //   Make a sticky navbar on scrolling
+    // ------------------------------------------------------ //
+    $(window).scroll(function () {
+
+        var body = $('body'),
+            stickyNavbar = $('nav.navbar-sticky'),
+            header = $('.header'),
+            topbar = $('.top-bar');
+
+        function makeItFixed(x) {
+            if ($(window).scrollTop() > x) {
+                stickyNavbar.addClass('fixed-top');
+                if (!header.hasClass('header-absolute')) {
+                    body.css('padding-top', stickyNavbar.outerHeight());
+                    body.data('smooth-scroll-offset', stickyNavbar.outerHeight());
+                }
+            } else {
+                stickyNavbar.removeClass('fixed-top');
+                body.css('padding-top', '0');
+            }
+        }
+
+        // if .top-bar exists, affix the navbar when we scroll past .top-bar
+        // else affix it immediately
+        if (stickyNavbar.length > 0 && topbar.length > 0) {
+            makeItFixed(topbar.outerHeight());
+        } else if (stickyNavbar.length > 0) {
+            makeItFixed(0);
+        }
+    });
